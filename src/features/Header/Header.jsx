0e858664fef2f792022b6a69eb4330ca8c01f497
shtepcell <vladislav.telichko@ya.ru>
2020-05-28 21:15:17 +0300

    Логотип и шапка

--- src/features/Header/Header.jsx
+++ src/features/Header/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { cn } from '@bem-react/classname';
 
 import { Icon } from '../../components/Icon';
+import { Link } from '../../components/Link';
+
 
 import './Header.scss';
 
@@ -11,6 +13,14 @@ export const Header = React.memo(() => (
     <header className={cnHeader()}>
         <nav className={cnHeader('Tab')}>
             <Icon className={cnHeader('Logo')} type="logo" />
+            <div className={cnHeader('Nav')}>
+                <Link className={cnHeader('Link')}>Интернет</Link>
+                <Link className={cnHeader('Link')}>IPTV</Link>
+                <Link className={cnHeader('Link')}>Доп. услуги</Link>
+                <Link className={cnHeader('Link')}>Оборудование</Link>
+                <Link className={cnHeader('Link')}>О нас</Link>
+                <Link className={cnHeader('Link')}>Документы</Link>
+            </div>
         </nav>
         <div className={cnHeader('Promo')} />
     </header>
